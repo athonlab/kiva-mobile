46ca613c990f8ad578e451293499e9622d0a4a46
Akshay Surve <akshay.surve@gmail.com>
2009-02-14 15:14:28 +0530

    Simple request

--- server/loans_recent.php
+++ server/loans_recent.php
@@ -1,3 +1,10 @@
 <?php
+
 // Code goes here!
+$page_no = $_GET['page'];
+$request_url = "http://api.kivaws.org/v1/loans/newest.json?page=$page_no";
+
+$json = file_get_contents($request_url);
+var_dump(json_decode($json, true));
+
 ?>
